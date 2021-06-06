0b4beaaea8a1ee6dfb8e6712bf3548888fb471c3
LintianShi <254656131@qq.com>
2021-06-06 16:31:46 +0800

    debug info output

--- src/test/TestMinimalRALinCheck.java
+++ src/test/TestMinimalRALinCheck.java
@@ -90,7 +90,7 @@ public class TestMinimalRALinCheck {
 
     public static void main(String[] args) throws Exception {
         RedisProcessor rp = new RedisProcessor();
-        rp.load("multi_client_trace/45-3");
+        rp.load("no_collision_trace/2");
         HappenBeforeGraph happenBeforeGraph = rp.generateProgram(new RRpq()).generateHappenBeforeGraph();
 
         TestMinimalRALinCheck.minimalExtensionRaLinCheck("result.txt", happenBeforeGraph, new RRpq());
--- src/validation/Validation.java
+++ src/validation/Validation.java
@@ -60,11 +60,11 @@ public class Validation {
                             String ret = adt.invoke(node.getInvocation());
                             if (ret != null) {
                                 rets.add(node.getId(), ret);
-//                                if (i == lin.size() - 1) {
-//                                    if (node.getInvocation().getRetValue().equals("50.0"))
-//                                    System.out.println(node.getInvocation().getMethodName() + ":"
-//                                            + node.getInvocation().getRetValue() + " -- " + ret);
-//                                }
+                                if (i == lin.size() - 1) {
+                                    if (!node.getInvocation().getRetValue().equals(ret))
+                                    System.out.println(node.getInvocation().getMethodName() + ":"
+                                            + node.getInvocation().getRetValue() + " -- " + ret);
+                                }
                             }
 
                         }
