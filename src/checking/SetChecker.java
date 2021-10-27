12920abf13d430a570c6326d087847cf9e050ff0
LintianShi <254656131@qq.com>
2021-10-27 16:08:36 +0800

    add a trace filter

--- src/checking/SetChecker.java
+++ src/checking/SetChecker.java
@@ -13,6 +13,7 @@ import java.io.FileReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -35,21 +36,8 @@ public class SetChecker {
     }
 
     public void testDataSet(List<String> dataset, boolean enableMulti) throws Exception {
-        int i = 0;
         for (String file : dataset) {
-            i++;
-            CountDownLatch countDownLatch = new CountDownLatch(1);
-            CheckerThread checkerThread = new CheckerThread(file, countDownLatch);
-            Thread thread = new Thread(checkerThread);
-            thread.start();
-            countDownLatch.await(180, TimeUnit.SECONDS);
-            thread.stop();
-            if (i % 10 == 0) {
-                System.out.println(i);
-            }
-            if (!checkerThread.result) {
-                System.out.println(file.toString() + ":" + checkerThread.result);
-            }
+            testTrace(file, true);
         }
     }
 
@@ -79,17 +67,30 @@ public class SetChecker {
         System.out.println();
     }
 
+    public List<String> filter(String filename) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+        List<String> result = new LinkedList<>();
+        String str = null;
+        while ((str = br.readLine()) != null) {
+            if (str.endsWith(":false"))
+                result.add(str.substring(0, str.lastIndexOf(':')));
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws Exception {
         //new SetChecker().testTrace("D:\\set311_with_size\\result\\set311_default_5_3_15_1634985195455.trc");
 //        new SetChecker().testDataSet("D:\\set311_with_size\\result", true);
-        BufferedReader br = new BufferedReader(new FileReader(new File("experiment_data/set311_complete_violation.txt")));
-        String str = null;
-        int i = 0;
-        while ((str = br.readLine()) != null) {
-            //if (i >= 34)
-                new SetChecker().testTrace(str, true);
-            //i++;
-        }
+//        BufferedReader br = new BufferedReader(new FileReader(new File("experiment_data/set311_complete_violation.txt")));
+//        String str = null;
+//        int i = 0;
+//        while ((str = br.readLine()) != null) {
+//            //if (i >= 34)
+//                new SetChecker().testTrace(str, true);
+//            //i++;
+//        }
+        List<String> r = new SetChecker().filter("experiment_data/set311_monotonic.txt");
+        new SetChecker().testDataSet(r, true);
     }
 }
 
